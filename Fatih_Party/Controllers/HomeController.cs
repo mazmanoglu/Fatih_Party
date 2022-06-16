d0e78283f5e4d563989735e8a779c3aaa2d18607
fatih ozer <fatihozer246@hotmail.com>
2022-06-16 11:45:05 +0200

    Add project files.

---
+++ Fatih_Party/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Fatih_Party.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fatih_Party.Controllers
+{
+	public class HomeController : Controller
+	{
+		public static List<VisitorsModel> visitors = new List<VisitorsModel>();
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(VisitorsModel visitor)
+		{
+			visitors.Add(visitor);
+			return RedirectToAction(nameof(Create));
+		}
+
+		public IActionResult Summary()
+		{
+			ViewBag.NumberofGuests = visitors.Count;
+			ViewBag.NumberOfFamilyGuests = visitors.Where(v => v.IsFamily).Count();
+			ViewBag.Youngest = visitors.Min(v => v.Age);
+			ViewBag.Oldest = visitors.Max(v => v.Age);
+
+			return View();
+		}
+	}
+}
---
+++ Fatih_Party/Models/VisitorsModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fatih_Party.Models
+{
+	public class VisitorsModel
+	{
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [Display(Name ="Your First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        [Display(Name = "Your Last Name")]
+        public string SecondName { get; set; }
+
+        [EmailAddress]
+        [Display(Name ="Your E-mail Address")]
+        public string EmailAdress { get; set; }
+
+        [Required]
+        [Phone]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Not a valid phone number")]
+        [Display(Name = "Your Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Range(18, 60, ErrorMessage = "Age must be between 18 - 60")]
+        [Display(Name ="Your Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name ="Your Home Address")]
+        public string Adress { get; set; }
+
+        [Required]
+        [Range(1000, 9992, ErrorMessage = "Postal Code must be between 1000 - 9992")]
+        [Display(Name = "Your Postal Code")]
+        public int PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "The City You Live")]
+        public string City { get; set; }
+
+        [Display(Name ="Are You a Family Member?")]
+        public bool IsFamily { get; set; }
+    }
+}
